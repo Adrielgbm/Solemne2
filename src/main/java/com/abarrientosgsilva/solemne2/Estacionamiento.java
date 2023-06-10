0f173b0c97c7e42645f09b45e1eef4cacfda6330
Adriel Barrientos <adrielgbm@gmail.com>
2023-06-10 13:19:57 -0400

    Nombre de la clase main modificado

--- src/main/java/com/abarrientosgsilva/solemne2/Estacionamiento.java
+++ src/main/java/com/abarrientosgsilva/solemne2/Estacionamiento.java
@@ -1,6 +1,6 @@
 package com.abarrientosgsilva.solemne2;
 
-public class Solemne2 {
+public class Estacionamiento {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
