9f46d2143631bfb68ff301feb4445208d4830edc
Adriel Barrientos <adrielgbm@gmail.com>
2023-06-10 18:58:07 -0400

    Add addCar method

--- src/main/java/com/Estacionamiento.java
+++ src/main/java/com/Estacionamiento.java
@@ -1,6 +1,11 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
+
+import com.abarrientosgsilva.solemne2.Automovil;
 
 public class Estacionamiento {
     private int maximumCapacity;
@@ -48,5 +53,46 @@ public class Estacionamiento {
                 + "]";
     }
 
+    public void addCar() throws IOException {
+        String patente, modelo, color, info;
+        char marca = 'C';
+        boolean check = false;
+        String regex = "^[A-Z]{4}[0-9]{2}$|^[0-9]{4}[A-Z]{2}$";
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        do {
+            System.out.print("Ingrese la patente del vehículo: ");
+            patente = br.readLine();
+            if(patente.matches(regex))
+                check = true;
+            else
+                System.out.println("La patente debe tener formato AAAA99 o 9999AA.");
+        } while(!check);
 
+        check = false;
+        regex = "^[CNHMF]$";
+
+        do {
+            System.out.print("\nMarcas:");
+            System.out.print("C -> Chevrolet");
+            System.out.print("N -> Nissan");
+            System.out.print("H -> Hyundai");
+            System.out.print("M -> Mercedes Benz");
+            System.out.print("F -> Ford");
+            System.out.print("\nIngrese la marca del vehículo: ");
+            info = br.readLine();
+            if(info.matches(regex)) {
+                marca = info.charAt(0);
+                check = true;
+            }
+            else
+                System.out.println("Debe ingresar la letra correspondiente a una de las siguientes marcas.");
+        } while(!check);
+
+        System.out.print("Ingrese el modelo del vehículo: ");
+        modelo = br.readLine();
+        System.out.print("Ingrese el color del vehículo: ");
+        color = br.readLine();
+        cars.push(new Automovil(patente, marca, modelo, color));
+        System.out.print("\nEl vehículo se ha estacionado correctamente.");
+    }
 }
