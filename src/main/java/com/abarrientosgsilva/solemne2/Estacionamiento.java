913e8300492801624fae04275a98958d1507b446
ig-byte <gabriel.silva.aravena@gmail.com>
2023-06-10 14:12:24 -0400

    testing cambie hello world por estacionamiento

--- src/main/java/com/abarrientosgsilva/solemne2/Estacionamiento.java
+++ src/main/java/com/abarrientosgsilva/solemne2/Estacionamiento.java
@@ -3,6 +3,6 @@ package com.abarrientosgsilva.solemne2;
 public class Estacionamiento {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Estacionamiento");
     }
 }
