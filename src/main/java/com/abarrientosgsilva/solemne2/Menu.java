71df35aff60e2a1d5681c0e11bdee77da62f8318
ig-byte <gabriel.silva.aravena@gmail.com>
2023-06-10 17:47:22 -0400

    cambio test

--- src/main/java/com/abarrientosgsilva/solemne2/Menu.java
+++ src/main/java/com/abarrientosgsilva/solemne2/Menu.java
@@ -3,6 +3,6 @@ package com.abarrientosgsilva.solemne2;
 public class Menu {
 
     public static void main(String[] args) {
-        System.out.println("Estacionamiento test");
+        System.out.println("Probando si ahora funciona - rev1");
     }
 }
