5c7f08143f8b15191dce90b4a97a958afedfe40a
Adriel Barrientos <adrielgbm@gmail.com>
2023-06-10 17:04:45 -0400

    Changes in Estacionamiento class

--- src/main/java/com/Estacionamiento.java
+++ src/main/java/com/Estacionamiento.java
@@ -1,5 +1,39 @@
 package com;
 
-public class Estacionamiento {
+import java.util.Stack;
 
+public class Estacionamiento {
+    private int maximumCapacity;
+    private int parkedCars;
+    private Stack cars;
+
+    public Estacionamiento(int parkedCars) {
+        cars = new Stack<>();
+    }
+
+    public int getmaximumCapacity() {
+        return maximumCapacity;
+    }
+
+    public void setmaximumCapacity(int maximumCapacity) {
+        this.maximumCapacity = 8;
+    }
+
+    public Stack getCars() {
+        return cars;
+    }
+
+    public void setCars(Stack cars) {
+        this.cars = cars;
+    }
+
+    public int getParkedCars() {
+        return parkedCars;
+    }
+
+    public void setParkedCars(int parkedCars) {
+        this.parkedCars = parkedCars;
+    }
+
+    
 }
