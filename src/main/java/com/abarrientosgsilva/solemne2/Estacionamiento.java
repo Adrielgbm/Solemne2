6d528447b634bdeb83ff706ff6d112fcfa1f03fb
ig-byte <gabriel.silva.aravena@gmail.com>
2023-06-10 14:19:51 -0400

    test

--- src/main/java/com/abarrientosgsilva/solemne2/Estacionamiento.java
+++ src/main/java/com/abarrientosgsilva/solemne2/Estacionamiento.java
@@ -3,6 +3,6 @@ package com.abarrientosgsilva.solemne2;
 public class Estacionamiento {
 
     public static void main(String[] args) {
-        System.out.println("Estacionamiento");
+        System.out.println("Estacionamiento test");
     }
 }
